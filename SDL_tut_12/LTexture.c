07c7fc0a566c8d1711d72a5746b57ee0e8733a98
yipidee <amjconnolly@hotmail.com>
2015-11-22 12:45:49 +0900

    add tutorial 12

---
+++ SDL_tut_12/LTexture.c
@@ -0,0 +1,94 @@
+#include "LTexture.h"
+
+LTexture LTexture_create()
+{
+    //create and initialize an LTexture struct
+    LTexture lt;
+    lt.mTexture = NULL;
+	lt.mWidth = 0;
+	lt.mHeight = 0;
+
+	return lt;
+}
+
+void LTexture_destroy(LTexture* lt)
+{
+    if( lt->mTexture != NULL )
+	{
+		SDL_DestroyTexture( lt->mTexture );
+		lt->mTexture = NULL;
+		lt->mWidth = 0;
+		lt->mHeight = 0;
+	}
+}
+
+bool LTexture_loadFromFile(LTexture *lt, SDL_Renderer* renderer, char* path )
+{
+	//Get rid of preexisting texture
+	LTexture_destroy(lt);
+
+	//The final texture
+	SDL_Texture* newTexture = NULL;
+
+	//Load image at specified path
+	SDL_Surface* loadedSurface = IMG_Load( path );
+	if( loadedSurface == NULL )
+	{
+		printf( "Unable to load image %s! SDL_image Error: %s\n", path, IMG_GetError() );
+	}
+	else
+	{
+		//Color key image
+		SDL_SetColorKey( loadedSurface, SDL_TRUE, SDL_MapRGB( loadedSurface->format, 0, 0xFF, 0xFF ) );
+
+		//Create texture from surface pixels
+        newTexture = SDL_CreateTextureFromSurface( renderer, loadedSurface );
+		if( newTexture == NULL )
+		{
+			printf( "Unable to create texture from %s! SDL Error: %s\n", path, SDL_GetError() );
+		}
+		else
+		{
+			//Get image dimensions
+			lt->mWidth = loadedSurface->w;
+			lt->mHeight = loadedSurface->h;
+		}
+
+		//Get rid of old loaded surface
+		SDL_FreeSurface( loadedSurface );
+	}
+
+	//Return success
+	lt->mTexture = newTexture;
+	return lt->mTexture != NULL;
+}
+
+void LTexture_setColor( LTexture* lt, Uint8 red, Uint8 green, Uint8 blue )
+{
+	//Modulate texture
+	SDL_SetTextureColorMod( lt->mTexture, red, green, blue );
+}
+
+void LTexture_render( LTexture* lt, SDL_Renderer* renderer, int x, int y, SDL_Rect* clip )
+{
+	//Set rendering space and render to screen
+	SDL_Rect renderQuad = { x, y, lt->mWidth, lt->mHeight };
+
+	if(clip != NULL)
+    {
+        renderQuad.w = clip->w;
+        renderQuad.h = clip->h;
+    }
+
+	SDL_RenderCopy( renderer, lt->mTexture, clip, &renderQuad );
+}
+
+int LTexture_getWidth(LTexture *lt)
+{
+    return lt->mWidth;
+}
+
+int LTexture_getHeight(LTexture *lt)
+{
+    return lt->mHeight;
+}
---
+++ SDL_tut_12/LTexture.h
@@ -0,0 +1,34 @@
+// A C implementation of Lazy Foo's LTexture C++ class
+
+#ifndef LTEXTURE_H
+#define LTEXTURE_H
+
+#include <SDL.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <SDL_image.h>
+
+// A structure to hold the LTEXTURE variables
+typedef struct LTexture
+{
+    SDL_Texture* mTexture;
+    int mWidth;
+    int mHeight;
+} LTexture;
+
+// Functions that operate on LTextures
+LTexture LTexture_create();
+
+void LTexture_destroy(LTexture* lt);
+
+bool LTexture_loadFromFile(LTexture *lt, SDL_Renderer* renderer, char* path );
+
+void LTexture_setColor( LTexture* lt, Uint8 red, Uint8 green, Uint8 blue );
+
+void LTexture_render( LTexture* lt, SDL_Renderer* renderer, int x, int y, SDL_Rect* clip );
+
+int LTexture_getWidth(LTexture *lt);
+
+int LTexture_getHeight(LTexture *lt);
+
+#endif // LTEXTURE_H

6e38db6dbcc897624e17a8cc64e684cd51a970be
yipidee <amjconnolly@hotmail.com>
2015-11-30 13:49:17 +0900

    add tutorial 15

---
+++ SDL_tut_15/15_rotation_and_flipping.c
@@ -0,0 +1,206 @@
+/*This source code copyrighted by Lazy Foo' Productions (2004-2015)
+and may not be redistributed without written permission.*/
+
+//Using SDL, SDL_image, standard IO, math, and strings
+#include <SDL.h>
+#include <SDL_image.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+#include "LTexture.h"
+
+//Colour definitions in RGBA list format for SDL renderer
+#define COLOUR_BLACK 0xFF, 0xFF, 0xFF, 0xFF
+
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+//Starts up SDL and creates window
+bool init();
+
+//Loads media
+bool loadMedia();
+
+//Frees media and shuts down SDL
+void close();
+
+//The window we'll be rendering to
+SDL_Window* gWindow = NULL;
+
+//The window renderer
+SDL_Renderer* gRenderer = NULL;
+
+//Scene texture
+LTexture gArrowTexture;
+
+bool init()
+{
+	//Initialization flag
+	bool success = true;
+
+	//Initialize SDL
+	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+	{
+		printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
+		success = false;
+	}
+	else
+	{
+		//Set texture filtering to linear
+		if( !SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "1" ) )
+		{
+			printf( "Warning: Linear texture filtering not enabled!" );
+		}
+
+		//Create window
+		gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+		if( gWindow == NULL )
+		{
+			printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
+			success = false;
+		}
+		else
+		{
+			//Create vsynced renderer for window
+			gRenderer = SDL_CreateRenderer( gWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC );
+			if( gRenderer == NULL )
+			{
+				printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
+				success = false;
+			}
+			else
+			{
+				//Initialize renderer color
+				SDL_SetRenderDrawColor( gRenderer, COLOUR_BLACK );
+
+				//Initialize PNG loading
+				int imgFlags = IMG_INIT_PNG;
+				if( !( IMG_Init( imgFlags ) & imgFlags ) )
+				{
+					printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
+					success = false;
+				}
+			}
+		}
+	}
+
+	return success;
+}
+
+bool loadMedia()
+{
+	//Loading success flag
+	bool success = true;
+
+	//Load arrow
+	gArrowTexture = LTexture_create();
+	if( !LTexture_loadFromFile(&gArrowTexture, gRenderer, "arrow.png" ) )
+	{
+		printf( "Failed to load arrow texture!\n" );
+		success = false;
+	}
+
+	return success;
+}
+
+void close()
+{
+	//Free loaded images
+	LTexture_destroy(&gArrowTexture);
+
+	//Destroy window
+	SDL_DestroyRenderer( gRenderer );
+	SDL_DestroyWindow( gWindow );
+	gWindow = NULL;
+	gRenderer = NULL;
+
+	//Quit SDL subsystems
+	IMG_Quit();
+	SDL_Quit();
+}
+
+int main( int argc, char* args[] )
+{
+	//Start up SDL and create window
+	if( !init() )
+	{
+		printf( "Failed to initialize!\n" );
+	}
+	else
+	{
+		//Load media
+		if( !loadMedia() )
+		{
+			printf( "Failed to load media!\n" );
+		}
+		else
+		{
+			//Main loop flag
+			bool quit = false;
+
+			//Event handler
+			SDL_Event e;
+
+			//Angle of rotation
+			double degrees = 0;
+
+			//Flip type
+			SDL_RendererFlip flipType = SDL_FLIP_NONE;
+
+			//While application is running
+			while( !quit )
+			{
+				//Handle events on queue
+				while( SDL_PollEvent( &e ) != 0 )
+				{
+					//User requests quit
+					if( e.type == SDL_QUIT )
+					{
+						quit = true;
+					}
+					else if( e.type == SDL_KEYDOWN )
+					{
+						switch( e.key.keysym.sym )
+						{
+							case SDLK_a:
+							degrees -= 60;
+							break;
+
+							case SDLK_d:
+							degrees += 60;
+							break;
+
+							case SDLK_q:
+							flipType = SDL_FLIP_HORIZONTAL;
+							break;
+
+							case SDLK_w:
+							flipType = SDL_FLIP_NONE;
+							break;
+
+							case SDLK_e:
+							flipType = SDL_FLIP_VERTICAL;
+							break;
+						}
+					}
+				}
+
+				//Clear screen
+				SDL_SetRenderDrawColor( gRenderer, COLOUR_BLACK );
+				SDL_RenderClear( gRenderer );
+
+				//Render arrow
+				LTexture_render(&gArrowTexture,gRenderer, ( SCREEN_WIDTH - LTexture_getWidth(&gArrowTexture) ) / 2, ( SCREEN_HEIGHT - LTexture_getHeight(&gArrowTexture) ) / 2, NULL, degrees, NULL, flipType );
+
+				//Update screen
+				SDL_RenderPresent( gRenderer );
+			}
+		}
+	}
+
+	//Free resources and close SDL
+	close();
+
+	return 0;
+}
